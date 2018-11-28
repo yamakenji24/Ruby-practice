d76b75a113d4126e4dcb764396b4fd64a0f3fcf9
Kenji Yamashita <s16t278@stu.kagawa-u.ac.jp>
2018-11-28 19:27:21 +0900

    fix where was pointed out

--- ls.rb
+++ ls.rb
@@ -1,15 +1,15 @@
 option = ARGV[0]
 def show_all
-  print"#{Dir.glob("*")}\n"
+  print "#{Dir.glob("*")}\n"
 end
 
 def show_dot
-  print Dir.glob(".*")
+  print "#{Dir.glob(".*")}\n"
 end
 
-def get_changetime
+def get_change_time
   Dir.glob("*") do |name|
-    s = File::Stat.new(name)
+    s = File.stat(name)
     print "#{s.mtime} #{name}\n"
   end
 end
@@ -18,7 +18,7 @@ case option
 when "-a"
   show_dot
 when "-t"
-  get_changetime
+  get_change_time
 else
   show_all
 end
