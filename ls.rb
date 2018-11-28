b8764204c71d7267650b708805f4c2e78809e78c
Kenji Yamashita <s16t278@stu.kagawa-u.ac.jp>
2018-11-28 18:55:57 +0900

    add ls options of -t

--- ls.rb
+++ ls.rb
@@ -1,16 +1,24 @@
 option = ARGV[0]
-
 def show_all
-  print Dir.glob("*")
+  print"#{Dir.glob("*")}\n"
 end
 
 def show_dot
   print Dir.glob(".*")
 end
 
+def get_changetime
+  Dir.glob("*") do |name|
+    s = File::Stat.new(name)
+    print "#{s.mtime} #{name}\n"
+  end
+end
+
 case option
 when "-a"
   show_dot
+when "-t"
+  get_changetime
 else
   show_all
 end
